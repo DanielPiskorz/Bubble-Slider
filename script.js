9708aad5a59b6c7a41642e4058a93026561fdb7e
DanielPiskorz <eel5426@gmail.com>
2017-08-01 16:16:02 +0200

    Divides code into functions.

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 $(document).ready(function () {
+    change(1);
     $(".bubble").click(function () {
-     	$(".bubble").css("background-color", "white");
-     	$(this).css("background-color", "#999");
-     	var id = parseInt(this.id.substring(1)); 
-     	var path = "img/img" + id + ".jpg";
-     	$("#selectedImg").fadeOut(500, function(){
-     		$(this).attr("src", path).bind("onreadystatechange load", function(){
-         		if (this.complete) $(this).fadeIn(500);
-          	});
-     	});
+     	var nr = parseInt(this.id.substring(1)); 
+     	change(nr)
     });
 });
+
+function change(number){
+    changeBubbles(number);
+    const path = "img/img" + number + ".jpg";
+    changeImage(path);
+}
+
+function changeBubbles(nr){
+    $(".bubble").css("background-color", "white");
+    const bbl = "#b" + nr;
+    $(bbl).css("background-color", "#999");
+}
+
+function changeImage(path){
+    $("#selectedImg").fadeOut(500, function(){
+        $(this).attr("src", path).bind("onreadystatechange load", function(){
+            if (this.complete) $(this).fadeIn(500);
+        });
+    });
+}
+
